1b1063502ea0f20e16e7e2694f8cf4e17e1f911b
Fran0616 <59398607+Fran0616@users.noreply.github.com>
2020-08-22 17:06:15 -0400

    Add files via upload

---
+++ negativeIndices.py
@@ -0,0 +1,7 @@
+ #Negative indices are legal 
+
+numbers = [111, 7, 2, 1]
+#An element with a index equal to =1 is the last one on the list
+print(numbers[-1])
+#Similarly, the element with an index equal to -2 is the one before the last on the list
+print(numbers[-2])
